49e1dece02b7dd82e3aeaab9b9946fdc02760805
Alexander <108187314+AlexanderPanevin@users.noreply.github.com>
2023-02-26 07:09:51 +0300

    добавил исключение ошибки ApiError и выход из программы sys.exit в "def write_msg()"

--- modules.py
+++ modules.py
@@ -1,13 +1,23 @@
 import vk_api
-from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.longpoll import VkLongPoll
 from settings import bot_token
 from random import randrange
+from vk_api.exceptions import ApiError
+import sys
 
 vk = vk_api.VkApi(token=bot_token)
 longpoll = VkLongPoll(vk)
 
 def write_msg(user_id, message):
-    vk.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': randrange(10 ** 7)})
+    try:
+        vk.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': randrange(10 ** 7)})
+    except ApiError:
+        for event in longpoll.listen():
+                      write_msg(event.user_id,'К сожаленю, получился слишком длинный список. Покажу в следующий раз. Перезапусти программу "бот"')
+                      sys.exit()
+        print('ошибка ApiError "слишком длинное сообщение"' )
+    else:
+        return
 
 def send_photo(user_id, message, attachments):
     try:
